03ecdd96d8bc596a10f131cf131882a01802b911
Jasper.iMac <zangshipeng1@huawei.com>
2022-07-08 15:16:44 +0800

    字符串

---
+++ 字符串/Test01.py
@@ -0,0 +1,14 @@
+a = 'Hello World'
+print('line 1 ',a)
+print('line 2 ',a[2])
+b = len(a)
+print('line 3 ',a[-1],a[-b])
+print('line 4 ',a+str(b),a*b)
+print('line 5','xxxx\
+xxx\
+xxxx')
+print('\a')
+
+'''
+注意特殊字符串
+'''
