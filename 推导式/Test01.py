73e6ec32a200e8cef54c727694962d30095b42e9
Jasper.iMac <zangshipeng1@huawei.com>
2022-07-08 12:04:18 +0800

    推导式 运算符

--- 推导式/Test01.py
+++ 推导式/Test01.py
@@ -6,7 +6,41 @@ Python 支持各种数据结构的推导式：
 集合(set)推导式
 元组(tuple)推导式
 '''
+print('---------------List---------------------')
 '''
-[表达式 for 变量 in 列表] 
-[表达式 for 变量 in 列表 if 条件]
+[out_exp_res for out_exp in input_list]
+[out_exp_res for out_exp in input_list if condition]
 '''
+names = ['aa','bbb','cccc','ddddd','xxxxxxxx']
+names = [name for name in names if len(name)>3]
+names1 = [name.upper() for name in names if len(name)>3]
+print('list--',names,names1)
+
+number_list1 = [0,1,2,3,4,5]
+print('list--',[num**2 for num in number_list1])
+print('list--',[num**2 for num in number_list1 if num>2])
+
+print('---------------Dictionary---------------')
+'''
+{ key_expr: value_expr for value in collection }
+{ key_expr: value_expr for value in collection if condition }
+'''
+dict_name = {'xiaoming','aHa','dingding','Weeeex','dingeeee'}
+print('dict--',{name:'name' for name in dict_name})
+'''生成的键值对，不要轻易key-value去取'''
+
+print('---------------Set----------------------')
+'''
+{ expression for item in Sequence }
+{ expression for item in Sequence if conditional }
+'''
+number_set = (1,2,3,4,5,6)
+print('set--',{num+1 for num in number_set if num>3})
+
+print('---------------Tuple--------------------')
+'''
+(expression for item in Sequence )
+(expression for item in Sequence if conditional )
+'''
+print('tuple--',(x**2 for x in range(0,10))) #返回生成器对象 需要序列化
+print('tuple--',tuple((x**2 for x in range(0,10))))
